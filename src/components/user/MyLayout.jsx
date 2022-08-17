0e2a17e107702c0056996bfb3b2dd5f027a25089
donamgunner <dydwns9310@gmail.com>
2022-08-17 19:11:43 +0900

    my Page done

--- src/components/user/MyLayout.jsx
+++ src/components/user/MyLayout.jsx
@@ -1,64 +1,82 @@
-import axios from 'axios';
-import React, { useState, useEffect} from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { H1 } from "../styled/Hn";
 import { H3_BOLD } from "../styled/Hn";
 import RESP from "../../server/response";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import Post from "../post/Post";
+import styled from "styled-components";
 
 function MyLayout(props) {
-    const [post, setPost] = useState([]);
-    console.log(post)
+  const [posts, setPosts] = useState([]);
+  const [numPosts, setNumPosts] = useState(0);
+  const [numComments, setNumComments] = useState(0);
+  
+  console.log(posts);
+  
+  const navigate = useNavigate();
+  
+  const getPosts = async (pageNum, pageLimit) => {
 
-    const navigate = useNavigate()
-
-    const getPost = async (pageNum,pageLimit) => {
     //     const {
     //         data : {
     //             result,
     //             data,
     //             status : { message },
     //         },
-    //     } = await axios.get(`http://localhost:3000/api/my`, 
+    //     } = await axios.get(`http://localhost:3000/api/my`,
     //     {
     //         headers: {
-    //             Authorization: localStorage.getItem("AccessToken"),
-    //             RefreshToken: localStorage.getItem("RefreshToken"),
+    //             Authorization: localStorage.getItem(“AccessToken”),
+    //             RefreshToken: localStorage.getItem(“RefreshToken”),
     //         }
     //     }
     // )
 
-        const {
-        result,
-        data,
-        status : { message },
-        } = RESP.MY_SUCCESS
-
-
-        if (!result) {
-        alert(message)
-        navigate("/home");
-        return
-        }
-        setPost(data)
-
+    const {
+      result,
+      data: { posts, numComments, numPosts },
+      status: { message },
+    } = RESP.MY_SUCCESS;
+    if (!result) {
+      alert(message);
+      navigate("/home");
+      return;
     }
-
-    useEffect (() => {
-        getPost(1,5)
-    },[])
-
-  const myPosts = post.map((props) => <div key={props.posts.id} {...props} />)
-
-
+    setPosts(posts);
+    setNumComments(numComments);
+    setNumPosts(numPosts);
+  };
+  useEffect(() => {
+    getPosts(1, 5);
+  }, []);
+  const myPosts = posts.map((post) => <Post key={post.id} {...post} />);
   return (
-   <> 
-    <H1>나의 활동 내역</H1>
-    <H3_BOLD>내가 작성한 게시글 : {post.numPosts}</H3_BOLD>
-    <H3_BOLD>내가 작성한 댓글 : {post.numComments}</H3_BOLD>
-    {myPosts}
-   </> 
-  )
+    <>
+    <Wrapper>
+      <TitleWrapper>
+      <H1>Activity History</H1>
+      </TitleWrapper>
+      <H3_BOLD>My Posts : {numPosts}</H3_BOLD>
+      <H3_BOLD>My Comments : {numComments}</H3_BOLD>
+      {myPosts}
+      </Wrapper> 
+    </>
+  );
 }
+export default MyLayout;
 
-export default MyLayout
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+  box-sizing: border-box;
+  overflow: scroll;
+`;
+
+const TitleWrapper = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 30px;
+`;
--- src/pages/user/My.jsx
+++ src/pages/user/My.jsx
@@ -7,29 +7,14 @@ import { H1 } from "../../components/styled/Hn";
 const My = (props) => {
   return (
     <>
-      <Wrapper>
+      <Layout>
         <Header />
-        <TitleWrapper>
-          <H1>My Page</H1>
-        </TitleWrapper>
        <MyLayout/>
-      </Wrapper>
+      </Layout>
     </>
   );
 };
 
 export default My;
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 50px;
-  box-sizing: border-box;
-  overflow: scroll;
-`;
 
-const TitleWrapper = styled.div`
-  box-sizing: border-box;
-  padding-bottom: 30px;
-`;
