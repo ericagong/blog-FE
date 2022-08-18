816229d2e679ff7779bd4676ad556112e64c7cdf
ericagong <48196721+ericagong@users.noreply.github.com>
2022-08-18 15:41:45 +0900

    delete logger

--- src/modules/store/ConfigStore.jsx
+++ src/modules/store/ConfigStore.jsx
@@ -1,14 +1,10 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import { createLogger } from "redux-logger";
+import { configureStore } from "@reduxjs/toolkit";
 
 import user from "../redux/user";
 import comment from "../redux/comment";
 
-const logger = createLogger();
-
 const store = configureStore({
   reducer: { user, comment },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
